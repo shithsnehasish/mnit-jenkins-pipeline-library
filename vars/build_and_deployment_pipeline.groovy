b72e527eaa21541e40ec18ca344afc0fbc32acd2
shithsnehasish <shithsnehasish@gmail.com>
2021-02-07 02:27:52 +0800

    Update build_and_deployment_pipeline.groovy

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -1,9 +1,13 @@
 def call(Map pipelineParams) {
+  env.BRANCH = pipelineParams.BRANCH
+  env.REPO = pipelineParams.REPO
   pipeline {
     node {
       stage("test") {
         sh '''
-          echo "testing"
+          echo "testing params --"
+          echo $REPO
+          echo $BRANCH
         '''
       }
     }
