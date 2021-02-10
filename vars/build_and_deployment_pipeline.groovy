358d9aca877bcfd8ebe247ef695490aaad93ea99
mniterp <78637357+mniterp@users.noreply.github.com>
2021-02-11 00:15:46 +0800

    Update build_and_deployment_pipeline.groovy

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -3,7 +3,28 @@ def call(Map pipelineParams) {
   env.REPO = pipelineParams.REPO
   pipeline {
     node {
-      stage("test") {
+      stage("Build Provsioning") {
+        sh '''
+          echo "testing params --"
+          echo $REPO
+          echo $BRANCH
+        '''
+      }
+      stage("Static Code Analysis") {
+        sh '''
+          echo "testing params --"
+          echo $REPO
+          echo $BRANCH
+        '''
+      }
+      stage("Upload Artifacts") {
+        sh '''
+          echo "testing params --"
+          echo $REPO
+          echo $BRANCH
+        '''
+      }
+      stage("Deploy") {
         sh '''
           echo "testing params --"
           echo $REPO
