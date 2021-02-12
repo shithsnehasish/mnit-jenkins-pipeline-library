8292e63beb71c76636fb2d4f130093bfbe18993e
snehasish <shithsnehasish@icloud.com>
2021-02-13 01:14:38 +0800

    initial code commit

--- src/com/mnit/util/checkoutSCM.groovy
+++ src/com/mnit/util/checkoutSCM.groovy
@@ -1,13 +1,11 @@
 package com.mnit.util
 
 def call(Map pipelineParams) {
-    SCM_URL="git@github.com:"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
+    env.SCM_URL="git@github.com:"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
     echo "Code checkout from SCM Repo"
-    checkout([$class: 'GitSCM', branches: [[name: "${pipelineParams.BRANCH}"]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${pipelineParams.REPO}"],
-        [$class: 'CheckoutOption', timeout: 10]],
-        submoduleCfg: [],
-        userRemoteConfigs: [[url: "${SCM_URL}"]]])
+    sh ''' 
+        rm -rf ${REPO}
+		git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+    ''' 
     echo "Checkout is completed!"
 }
