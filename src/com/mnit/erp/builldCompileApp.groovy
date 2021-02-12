9d9afe4b5a4c77ea48a2ae09b964a1a4bd81efa4
snehasish <shithsnehasish@icloud.com>
2021-02-13 01:19:48 +0800

    initial code commit

--- src/com/mnit/erp/builldCompileApp.groovy
+++ src/com/mnit/erp/builldCompileApp.groovy
@@ -5,7 +5,7 @@ def call(Map pipelineParams) {
     {
         sh '''
             cd $REPO
-            deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}-Dmaven.test.skip=true
+            mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}-Dmaven.test.skip=true
         '''
     }
     else if(pipelineParams.APP_TYPE == "NODE")
