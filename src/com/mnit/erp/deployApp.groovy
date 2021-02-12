e3f467e8d73bd9847138e43c735a01d247b6f9c6
snehasish <shithsnehasish@icloud.com>
2021-02-13 01:56:32 +0800

    initial code commit

--- src/com/mnit/erp/deployApp.groovy
+++ src/com/mnit/erp/deployApp.groovy
@@ -12,6 +12,7 @@ def call(Map pipelineParams) {
         sed -i "s;%CONTAINER_PORT%;${CONTAINER_PORT};" app-service.yaml
         sed -i "s;%HOST_PORT%;${HOST_PORT};" app-service.yaml
         sed -i "s;%HOST_IP%;${HOST_IP};" app-service.yaml
+        sed -i "s;%REPO%;${HOST_IP};" app-service.yaml
         echo Curiosity4ERP# | sudo -S kubectl apply -f app-service.yaml
     '''
 }
