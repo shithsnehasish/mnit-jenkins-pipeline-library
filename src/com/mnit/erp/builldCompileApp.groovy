ea7ef82fe76f02c72074d80def845a26069c6fb7
snehasish <shithsnehasish@icloud.com>
2021-02-13 01:18:28 +0800

    initial code commit

--- src/com/mnit/erp/builldCompileApp.groovy
+++ src/com/mnit/erp/builldCompileApp.groovy
@@ -4,6 +4,7 @@ def call(Map pipelineParams) {
     if(pipelineParams.APP_TYPE == "JAVA")
     {
         sh '''
+            cd $REPO
             deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}-Dmaven.test.skip=true
         '''
     }
