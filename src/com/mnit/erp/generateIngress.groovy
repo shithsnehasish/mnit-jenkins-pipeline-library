9dbb7576f029c5af35e7fc1fbe68df08012b6e14
snehasish <shithsnehasish@icloud.com>
2021-02-17 02:35:42 +0800

    added all jpi

--- src/com/mnit/erp/generateIngress.groovy
+++ src/com/mnit/erp/generateIngress.groovy
@@ -14,23 +14,11 @@ metadata:
   annotations:
     kubernetes.io/ingress.class: nginx
     nginx.ingress.kubernetes.io/ssl-redirect: "false"
-    nginx.ingress.kubernetes.io/use-regex: "true"
-    nginx.ingress.kubernetes.io/rewrite-target: /$1
 spec:
   rules:
   - http:
       paths:" >> ${WORKSPACE}/ingress.yaml
 '''
-svcs.each {
-  env.SVC = it
-sh '''
-echo "
-      - backend:
-          serviceName: $ENVIRONMENT-$SVC-svc
-          servicePort: 80
-        path: /$SVC(/|$)(.*)" >> ${WORKSPACE}/ingress.yaml
-'''
-}
 sh '''
 echo "
   - host: $DNS # change the IP address here
