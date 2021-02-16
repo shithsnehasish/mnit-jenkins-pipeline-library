7c28bff0ecad2fe454891b0c38462a8b3b0ddb06
snehasish <shithsnehasish@icloud.com>
2021-02-16 18:57:08 +0800

    added all jpi

--- src/com/mnit/erp/kubeConfigMap.groovy
+++ src/com/mnit/erp/kubeConfigMap.groovy
@@ -13,6 +13,7 @@ def call(Map pipelineParams) {
             sed -i "s;%HOST_IP%;${HOST_IP};" configMap.yaml
             sed -i "s;%DB_NAME%;${DB_NAME};" configMap.yaml
             sed -i "s;%ENVIRONMENT%;${ENVIRONMENT};" configMap.yaml
+            echo ${user}
             echo ${pwd} | sudo -S kubectl apply -f configMap.yaml
         '''
     }
