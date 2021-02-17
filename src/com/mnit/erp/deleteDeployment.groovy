82e38b2a72b85a13f75ca7e151e30a2c83fd168b
snehasish <shithsnehasish@icloud.com>
2021-02-17 10:34:47 +0800

    added all jpi

--- src/com/mnit/erp/deleteDeployment.groovy
+++ src/com/mnit/erp/deleteDeployment.groovy
@@ -2,9 +2,9 @@ package com.mnit.erp
 
 def call(Map pipelineParams) {
 
-    withCredentials([usernamePassword(credentialsId: '$ENVIRONMENT-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
+    withCredentials([usernamePassword(credentialsId: 'dev-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
         sh '''
-            echo $pwd | sudo -S kubectl delete deployment $ENVIRONMENT-$APP_NAME-deployment -n $NAMESPACE
+            echo Curiosity4ERP# | sudo -S kubectl delete deployment $ENVIRONMENT-$APP_NAME-deployment -n $NAMESPACE
         '''
     }
 }
--- src/com/mnit/erp/deleteIngress.groovy
+++ src/com/mnit/erp/deleteIngress.groovy
@@ -2,9 +2,9 @@ package com.mnit.erp
 
 def call(Map pipelineParams) {
 
-    withCredentials([usernamePassword(credentialsId: '$ENVIRONMENT-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
+    withCredentials([usernamePassword(credentialsId: 'dev-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
         sh '''
-            echo $pwd | sudo -S kubectl delete ingress $ENVIRONMENT-$NAMESPACE-ingress -n $NAMESPACE
+            echo Curiosity4ERP# | sudo -S kubectl delete ingress $ENVIRONMENT-$NAMESPACE-ingress -n $NAMESPACE
         '''
     }
 }
