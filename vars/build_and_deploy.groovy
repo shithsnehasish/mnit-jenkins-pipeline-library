5daf9d33e9a87ebf409c4b7bfa73384977e6ea24
shithsns <63468293+shithsns@users.noreply.github.com>
2021-07-11 13:31:32 +0800

    Create build_and_deploy.groovy

---
+++ vars/build_and_deploy.groovy
@@ -0,0 +1,51 @@
+
+def call(Map pipelineParams) {
+  try{
+    timeout(time: 60, unit: 'MINUTES') {
+      pipeline {
+        
+        node {
+          stage("Code Checkout") {
+            sh '''
+              git clone https://github.com/jglick/simple-maven-project-with-tests.git
+            '''
+          }
+          stage("Build") {
+            sh '''
+              /opt/apache-maven-3.8.1/mvn clean install
+            '''
+          }
+          stage("Static Code Analysis") {
+            sh '''
+              echo "Static Code Analysis"
+            '''
+          }
+          stage("Delete Previous Deployment") {
+            sh '''
+              echo "Delete Previous Deployment"
+            '''
+          }  
+          stage("Deploy") {
+            sh '''
+              echo "Deployment"
+            '''
+          }
+        }
+        if(pipelineParams.EMAIL_TO_LIST?.trim()){   
+          echo "email send enabled"   
+          //new sendEmail().call(pipelineParams,"SUCCESS")   
+        } 
+
+      }
+    }
+  }catch (err) {
+    echo "in catch block" 
+    echo "Caught: ${err}" 
+    currentBuild.result = 'FAILURE' 
+    if(pipelineParams.EMAIL_TO_LIST?.trim()){   
+      echo "email send enabled"   
+      //sendEmail().call(pipelineParams,"FAILURE")   
+    }  
+    throw err
+  }
+}
